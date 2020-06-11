2294a847b96d0b4e3847925543de1c9a932a4f63
Tadhg Lewis <tadhg.lewis@outlook.com>
2020-06-11 18:42:07 +1000

    Basic styling for incident

--- src/components/incidents/incident/index.js
+++ src/components/incidents/incident/index.js
@@ -2,9 +2,32 @@ import React from "react";
 import styled from "styled-components";
 
 const Incident = styled.div`
-  background-color: red;
+  background-color: white;
+  border-radius: 3px;
+  padding: 16px;
+  box-shadow: 0px 0px 33px -32px rgba(0, 0, 0, 0.75);
+
+  :not(:last-child) {
+    margin-bottom: 16px;
+  }
+`;
+
+const Created = styled.div`
+  margin: 0;
+  font-weight: bold;
+  margin-bottom: 16px;
+`;
+
+const Comment = styled.div`
+  white-space: pre;
 `;
 
 export default ({ incident }) => {
-  return <Incident>{incident.title}</Incident>;
+  console.log(incident);
+  return (
+    <Incident>
+      <Created>{new Date(incident.created_at).toUTCString()}</Created>
+      <Comment>{incident.body}</Comment>
+    </Incident>
+  );
 };
--- src/components/incidents/index.js
+++ src/components/incidents/index.js
@@ -4,13 +4,17 @@ import Incident from "./incident";
 
 const Container = styled.div`
   margin: 32px auto 0 auto;
-  max-width: 1009px;
-  padding: 16px;
+  max-width: 1040px;
+`;
+
+const Title = styled.h3`
+  padding: 0 16px;
 `;
 
 export default ({ loading, incidents }) => {
   return (
     <Container>
+      <Title>Incidents</Title>
       {!loading
         ? incidents.map((incident) => (
             <Incident key={incident.id} incident={incident} />
