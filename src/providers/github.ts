d348942911dc5e2c21a614e22298381286e50b57
Tadhg Lewis <tadhg.lewis@outlook.com>
2024-05-15 12:59:07 +1000

    Fix linting

--- src/providers/github.ts
+++ src/providers/github.ts
@@ -5,26 +5,43 @@ export const github: Provider = {
     {
       id: "1",
       name: "API",
+      status: "operational",
     },
     {
       id: "2",
       name: "Website",
+      status: "performanceIssues",
     },
     {
       id: "3",
       name: "App",
+      status: "partialOutage",
     },
     {
       id: "4",
       name: "Payments",
+      status: "majorOutage",
+    },
+    {
+      id: "5",
+      name: "Support",
+      status: "unknown",
     },
   ],
   getIncidents: () => [
     {
+      id: "1",
       title: "Major service outage",
       description: "This is a major service outage",
       createdAt: "2024-05-13T08:55:04.355Z",
-      status: "open",
+      active: true,
+    },
+    {
+      id: "2",
+      title: "Partial payments outage",
+      description: "This is a partial payments outage",
+      createdAt: "2024-05-13T08:55:04.355Z",
+      active: false,
     },
   ],
 };
