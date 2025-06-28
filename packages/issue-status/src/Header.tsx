a47fe02f89d7e5c3446dd811d7cc714e493f224a
Tadhg Lewis <tadhg.lewis@outlook.com>
2025-06-28 14:11:54 +1000

    Fix

--- packages/issue-status/src/Header.tsx
+++ packages/issue-status/src/Header.tsx
@@ -4,7 +4,7 @@ export const Header = () => (
   <div className="flex items-center justify-center gap-4">
     {config.logo ? (
       <img
-        src="/logo.svg"
+        src="logo.svg"
         alt={`${config.name} logo`}
         className="max-h-30 w-auto"
       />
